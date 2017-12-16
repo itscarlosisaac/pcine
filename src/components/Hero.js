2371a7b759c142633a92606f6da4c2487ee6c018
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-16 15:56:53 -0400

    Added some main components

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Hero extends Component {
+    render () {
+        return (
+            <div>
+                HERO
+            </div>
+        )
+    }
+}
+
+export default Hero
