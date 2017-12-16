5f39c9ebf61ba98bde8a73dd74e806afff04e1ad
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-16 15:57:13 -0400

    Added new components to homepage.

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,17 +4,24 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // Importing Common Components
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import MovieList from '../components/MovieList'
+import Hero from '../components/Hero'
 
 // Importing Page Components
 import HomePage from '../components/pages/HomePage'
 import ContactPage from '../components/pages/ContactPage'
 import NotFoundPage from '../components/pages/NotFoundPage'
 import TheathersPage from '../components/pages/TheathersPage'
+
 // App Router
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
+            <Hero />
+            <div className="app__main__content">
+                <MovieList />
+            </div>
             <div>
                 <Switch>
                     <Route exact={true} path="/" component={HomePage} />
