d3535d74a2651b1217a397f7ad17cc5942908ad5
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-16 15:57:29 -0400

    Created basic data fetch.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Navigation from './Nav';
+import getData from '../fecth/fetch.data'
+
 
 class Header extends React.Component{
     render(){
+        console.log( getData() )
         return(
             <header>
                 HEADER COMPONENT
