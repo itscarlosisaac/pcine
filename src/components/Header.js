3cd4fd4ceadea34276faca3fb04b3a3636524a37
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-16 13:18:51 -0400

    Added header and footer component files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                HEADER COMPONENT
+            </header>
+        )
+    }
+}
+
+export default Header;
