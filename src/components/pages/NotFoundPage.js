bdfdad442cb9ee2dfb4d3bea55c81551fa091c41
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-16 13:23:35 -0400

    Added main pages components.

---
+++ src/components/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NotFoundPage extends Component {
+    render () {
+        return (
+            <div>
+                Sorry you arrived at the wrong page.
+            </div>
+        )
+    }
+}
+
+export default NotFoundPage
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -6,14 +6,20 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 // Importing Page Components
+import HomePage from '../components/pages/HomePage'
+import ContactPage from '../components/pages/ContactPage'
+import NotFoundPage from '../components/pages/NotFoundPage'
 
+// App Router
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
             <div>
                 <Switch>
-                    <Route exact={true} path="/" />
+                    <Route exact={true} path="/" component={HomePage} />
+                    <Route path='/contact' component={ContactPage} />
+                    <Route component={NotFoundPage} />
                 </Switch>
             </div>
             <Footer />
