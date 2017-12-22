8d99f72ff6f02184d4e24c351f3bf4cdd3b96b73
Carlos Martinez <itscarlosisaac@gmail.com>
2017-12-22 18:25:53 -0400

    Added typography styles.

--- src/app.js
+++ src/app.js
@@ -5,5 +5,6 @@ import './style/styles.scss';
 // Importing Components
 import AppRouter from './router/AppRouter'
 
+
 const root = document.getElementById("root");
 ReactDOM.render(<AppRouter /> , root )
