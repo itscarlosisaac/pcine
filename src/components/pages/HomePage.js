ba0ba1cadeda01c09e8c6bd75d267d1d730924bf
Carlos Martinez <itscarlosisaac@gmail.com>
2018-01-02 15:11:00 -0400

    Refactor to have the movie list in home page.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import MovieList from '../MovieList'
+import Hero from '../Hero'
+import Sidebar from '../Sidebar'
+import api from '../../utils/api'
 
 class HomePage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            movieData: [],
+            filteredData: [],
+            grid: 'grid-4_lg-4_md-4_sm-3_xs-2'
+        }
+    }
+    componentDidMount(){
+        api.fetchMovies().then( (movieData) => { 
+            this.setState( (prev, next) => ({
+                movieData: movieData,
+                filteredData: movieData
+            }))
+        });
+    }
+    handleSearch(val){
+        this.setState( ( prevState, nextState ) => ({
+            filteredData: api.filterMovies( val,  prevState.movieData )    
+        }))
+    }
     render () {
         return (
             <div>
-                HOME
+                <Hero handleSearch={this.handleSearch.bind(this)} />
+                <div className="app__main__content">
+                    <MovieList movieData={this.state.filteredData}  grid={this.state.grid} />
+                    <Sidebar />
+                </div>
             </div>
         )
     }
